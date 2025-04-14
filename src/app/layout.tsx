0f729a8a00a3c3d86ec23adc171bce09a282a966
Xyves <xodrixx@protonmail.com>
2025-04-14 22:07:13 +0200

    Update Layouts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,22 @@
+'use client';
+
 import '@/styles/globals.css';
 
 import { Inter } from 'next/font/google';
+import { getSession, SessionProvider } from 'next-auth/react';
 
 import MainFooter from '@/components/Footer';
 import { QueryProvider } from '@/providers/query';
 import type { ChildrenProps } from '@/types';
 import Heading from '@/components/Header/Heading';
 
-export const metadata = {
-  description:
-    'A highly opinionated and complete starter for Next.js projects ready to production. Includes Typescript, Styled Components, Prettier, ESLint, Husky, SEO, and more.',
-  keywords:
-    'next, starter, typescript, tailwind css, prettier, eslint, husky, seo',
-  title: 'Lumeo',
-};
+// export const metadata = {
+//   description:
+//     'A highly opinionated and complete starter for Next.js projects ready to production. Includes Typescript, Styled Components, Prettier, ESLint, Husky, SEO, and more.',
+//   keywords:
+//     'next, starter, typescript, tailwind css, prettier, eslint, husky, seo',
+//   title: 'Lumeo',
+// };
 
 const inter = Inter({
   subsets: ['latin'],
@@ -21,17 +24,22 @@ const inter = Inter({
   adjustFontFallback: false,
 });
 
-export default async function RootLayout({ children }: ChildrenProps) {
+export default function RootLayout({ children }: ChildrenProps) {
+  // const session = await getSession();
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-[#171006] overflow-hidden`}>
-        <div className="h-full flex flex-col justify-between w-[68%] mx-auto ">
-          <Heading title="NeonSphere " />
+      <body
+        className={`${inter.className} bg-[#171006] overflow-hidden min-h-full`}
+      >
+        {/* <SessionProvider session={session}> */}
+        <div className="h-full flex flex-col justify-between  mx-auto ">
+          {/* <Heading title="NeonSphere " /> */}
           <section className="flex-1 h-full">
             <QueryProvider>{children}</QueryProvider>
           </section>
           {/* <MainFooter /> */}
         </div>
+        {/* </SessionProvider> */}
       </body>
     </html>
   );
--- src/layouts/MainLayout/MainLayout.tsx
+++ src/layouts/MainLayout/MainLayout.tsx
@@ -1,11 +1,16 @@
 import Sidebar from '@/components/Sidebar/Sidebar';
+import { RefProvider } from '@/context/RefContext';
 import type { ChildrenProps } from '@/types';
 
 export default function MainLayout({ children }: ChildrenProps) {
   return (
-    <main className="flex flex-row relative mx-auto  [&>*]:w-1/4 h-full">
-      <Sidebar />
-      {children}
-    </main>
+    <RefProvider>
+      <main className="flex flex-row relative mx-auto h-full min-h-screen [&>*]:w-1/4  w-[68%]">
+        <div className=" h-auto flex">
+          <Sidebar />
+        </div>
+        {children}
+      </main>
+    </RefProvider>
   );
 }
