6a73d1ce8e7dab894c343106f6b7e3fd800c9ab2
Xyves <xodrixx@protonmail.com>
2025-04-20 21:51:37 +0200

    Create Post, Author and session TS interfaces

--- src/types/index.ts
+++ src/types/index.ts
@@ -32,6 +32,35 @@ export interface CurrentUserProps {
     isAdmin: boolean;
   } | null;
 }
+type likesType = {
+  id: string;
+};
+type authorType = {
+  id: string;
+  image: string;
+  name: string;
+};
+
+export interface PostInterface {
+  author?: authorType;
+  likes?: likesType[];
+  id: string;
+  date: string;
+  content: string;
+  image_url: string;
+  commentCount: number;
+  likeCount: number;
+}
+interface User {
+  id: string;
+  name: string;
+  email: string;
+}
+
+export interface SessionInterface {
+  user: User;
+  status?: boolean;
+}
 export type LoginSchema = {
   name: string;
   password: string;
