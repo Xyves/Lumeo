4a0dac315ee101e7a2bafc99a526a78be0ce03f1
Xyves <xodrixx@protonmail.com>
2025-04-22 21:17:52 +0200

    Feat: Create explore page

--- src/app/(routes)/explore/page.tsx
+++ src/app/(routes)/explore/page.tsx
@@ -1,9 +1,67 @@
+'use client';
+
+import { useEffect, useMemo, useState } from 'react';
+import { usePathname } from 'next/navigation';
+import { useSession } from 'next-auth/react';
+import { OrbitProgress } from 'react-loading-indicators';
+
 import MainLayout from '@/layouts/MainLayout/MainLayout';
+import { usePostLoader } from '@/hooks/usePostLoader';
+import NewFollows from '@/components/Aside/NewFollowsers';
+import PostsList from '@/components/Feed/PostsList';
+import CreatePost from '@/components/Feed/CreatePost';
 
 export default function Explore() {
+  const [loading, setLoading] = useState(false);
+  const [posts, setPosts] = useState<PostInterface[]>([]);
+  const [start, setStart] = useState<number>(0);
+  const { data: session } = useSession();
+  const { loadPosts, updateStart } = usePostLoader();
+  const feedType = 'explore';
+
+  useEffect(() => {
+    if (start !== 0) {
+      if (session && session.user) {
+        // delay(200);
+        loadPosts({
+          start,
+          userId: session.user.id,
+          setPosts,
+          setLoading,
+          feedType,
+        });
+      }
+    }
+  }, [start]);
+
+  useEffect(() => {
+    loadPosts({
+      start: 0,
+      userId: session.user.id,
+      setPosts,
+      setLoading,
+      feedType,
+    });
+  }, []);
+
+  const memoizedPosts = useMemo(() => posts, [posts]);
   return (
     <MainLayout>
-      <div>Explore</div>
+      <div className=" w-2/5 bg-[#1f1e1c] p-7 overflow-y-auto mb-24 mx-auto">
+        <CreatePost setPosts={setPosts} />
+        <PostsList
+          memoizedPosts={memoizedPosts}
+          setStart={setStart}
+          handleUpdateStart={updateStart}
+        />
+        {loading && (
+          <div className="loading-spinner flex justify-center">
+            <OrbitProgress variant="track-disc" speedPlus={2} easing="linear" />
+          </div>
+        )}
+      </div>
+      <NewFollows />
+      {/* <Search /> */}
     </MainLayout>
   );
 }
