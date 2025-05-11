9f5175159e9a99475b062d1c24d851b138f0320e
Xyves <xodrixx@protonmail.com>
2025-05-11 20:39:50 +0200

    Feat: Create form validation on input change & show custom errors & add change password visibility button

--- src/app/(routes)/signup/page.tsx
+++ src/app/(routes)/signup/page.tsx
@@ -1,4 +1,9 @@
-import React from 'react';
+'use client';
+
+import React, { useEffect } from 'react';
+import type { z } from 'zod';
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
 
 import SignUp from '@/components/pages/SignUp';
 import AuthLayout from '@/layouts/MainLayout/AuthLayout';
@@ -6,6 +11,13 @@ import HomeContent from '@/components/Home/HomeContent';
 import RegisterForm from '@/components/Home/RegisterForm';
 
 export default function SignUpPage() {
+  const router = useRouter();
+  const { status } = useSession();
+  useEffect(() => {
+    if (status !== 'unauthenticated') {
+      router.push('/feed');
+    }
+  }, [status, router]);
   return (
     <AuthLayout>
       <div className="lg:flex-row  max-sm:flex-col  md:flex-row flex w-full min-h-screen">
--- src/components/Home/RegisterForm.tsx
+++ src/components/Home/RegisterForm.tsx
@@ -3,31 +3,78 @@
 import React, { useState } from 'react';
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
+import { z } from 'zod';
 
-import type { RegisterType } from '@/schema';
-import { RegisterSchema } from '@/schema';
+import { RegisterSchema, RegisterType } from '@/schema';
 
+type FormData = z.infer<typeof RegisterSchema>;
+type FormErrors = Partial<Record<keyof FormData, string[]>>;
 export default function RegisterForm() {
+  const [passwordVisible, setPasswordVisible] = useState(false);
+
+  const [errors, setErrors] = useState<FormErrors>({});
   const router = useRouter();
-  const [data, setData] = useState<RegisterType>({
+  const [data, setData] = useState<FormData>({
     name: '',
     email: '',
     password: '',
   });
-
+  const togglePasswordVisibility = () => {
+    setPasswordVisible(prev => !prev);
+  };
+  const validateForm = (data: FormData) => {
+    try {
+      RegisterSchema.parse(data);
+      return {};
+    } catch (error) {
+      if (error instanceof z.ZodError) {
+        return error.flatten().fieldErrors;
+      }
+      return {};
+    }
+  };
   const registerUser = async (e: any) => {
     e.preventDefault();
+    const newErrors = validateForm(data);
+    setErrors(newErrors);
     console.log({ data });
-    const response = await fetch('/api/register', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(data),
-    });
-    const userInfo = await response.json();
-    console.log(userInfo);
-    // router.push('/');
+    if (Object.keys(newErrors).length === 0) {
+      const response = await fetch('/api/register', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+      });
+      if (response.ok) {
+        const userInfo = await response.json();
+
+        router.push('/');
+      } else {
+        console.error('Network error or server issue');
+      }
+    }
+  };
+  const handleFieldChange = (
+    e: React.ChangeEvent<HTMLInputElement>,
+    data: FormData,
+    setData: React.Dispatch<React.SetStateAction<FormData>>,
+    setErrors: React.Dispatch<React.SetStateAction<FormErrors>>
+  ) => {
+    const { name, value } = e.target;
+
+    const updated = { ...data, [name]: value };
+    setData(updated);
+
+    const fieldSchema = (RegisterSchema.shape as Record<string, z.ZodTypeAny>)[
+      name
+    ];
+    const result = fieldSchema.safeParse(value);
+
+    setErrors(prev => ({
+      ...prev,
+      [name]: result.success ? undefined : result.error.issues[0].message,
+    }));
   };
   return (
     <div className="  lg:w-2/5  sm:w-full md:w-full bg-white rounded-lg shadow dark:border md:mt-0    dark:bg-gray-800 justify-center items-center flex dark:border-gray-700 ">
@@ -55,10 +102,11 @@ export default function RegisterForm() {
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
               placeholder="Enter your username"
               required
-              onChange={e => {
-                setData({ ...data, name: e.target.value });
-              }}
+              onChange={e => handleFieldChange(e, data, setData, setErrors)}
             />
+            {errors.name && (
+              <p className="text-red-500 text-sm mt-1">{errors.name}</p>
+            )}
           </div>
           <div>
             <label
@@ -75,10 +123,11 @@ export default function RegisterForm() {
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
               placeholder="Enter your email"
               required
-              onChange={e => {
-                setData({ ...data, email: e.target.value });
-              }}
+              onChange={e => handleFieldChange(e, data, setData, setErrors)}
             />
+            {errors.email && (
+              <p className="text-red-500 text-sm mt-1">{errors.email}</p>
+            )}
           </div>
           <div>
             <label
@@ -87,18 +136,32 @@ export default function RegisterForm() {
             >
               Password
             </label>
-            <input
-              type="password"
-              name="password"
-              id="password"
-              value={data.password}
-              onChange={e => {
-                setData({ ...data, password: e.target.value });
-              }}
-              placeholder="••••••••"
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              required
-            />
+            <div className="relative">
+              <input
+                type={passwordVisible ? 'text' : 'password'}
+                name="password"
+                id="password"
+                value={data.password}
+                onChange={e => handleFieldChange(e, data, setData, setErrors)}
+                placeholder="••••••••"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                required
+              />
+              <button
+                type="button"
+                onClick={togglePasswordVisibility}
+                className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-500 dark:text-white"
+              >
+                {passwordVisible ? (
+                  <span>Hide</span> // Alternatively, use an eye icon here
+                ) : (
+                  <span>Show</span> // Alternatively, use an eye-slash icon here
+                )}
+              </button>
+            </div>
+            {errors.password && (
+              <p className="text-red-500 text-sm mt-1">{errors.password}</p>
+            )}
           </div>
 
           <button
