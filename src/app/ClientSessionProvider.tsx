5e438fb5c766cf350d3c1fd57f67a87f134d99ce
Xyves <xodrixx@protonmail.com>
2025-05-25 20:00:03 +0200

    Chore: add typescript type to client session provider

--- src/app/ClientSessionProvider.tsx
+++ src/app/ClientSessionProvider.tsx
@@ -1,8 +1,17 @@
 'use client';
 
 import { SessionProvider, useSession } from 'next-auth/react';
+import type { Session } from 'next-auth';
 
-const ClientSessionProvider = ({ session, children }: any) => {
+type ClientSessionProviderProps = {
+  session: Session | null;
+  children: React.ReactNode;
+};
+
+const ClientSessionProvider = ({
+  session,
+  children,
+}: ClientSessionProviderProps) => {
   return <SessionProvider session={session}>{children}</SessionProvider>;
 };
 export default ClientSessionProvider;
