36f4be1d2e26559828ed59a5dbcc7613748f3ae0
Xyves <xodrixx@protonmail.com>
2025-05-30 08:29:41 +0200

    Types: update and improve TS type definitions

--- src/types/index.ts
+++ src/types/index.ts
@@ -21,44 +21,41 @@ export type IToken = {
 
 export interface CurrentUserProps {
   currentUser?: {
-    createdAt: string;
-    updatedAt: string;
-    emailVerified: string | null | undefined;
     id: string;
     name: string;
     email: string | null;
     image: string | null;
     password: string | null;
+    emailVerified: string | null | undefined;
     isAdmin: boolean;
+    createdAt: string;
+    updatedAt: string;
   } | null;
 }
+
 export type searchUserType = Pick<
   NonNullable<CurrentUserProps['currentUser']>,
   'id' | 'name' | 'image' | 'createdAt' | 'updatedAt'
 >;
+
 export type RegisterType = Pick<
   NonNullable<CurrentUserProps['currentUser']>,
   'name' | 'email' | 'password'
 >;
+
 export type FollowerType = {
   id: string;
   name: string;
   profile_url: string;
   image: string;
 };
-type likesType = {
-  id: string;
-};
-type authorType = {
-  id: string;
-  image: string;
-  name: string;
-};
+
 export type editProfileType = {
   name: string;
   email: string;
   file: File;
 };
+
 export type getUsersProps = {
   input?: string | null;
   userId?: string | null;
@@ -69,6 +66,7 @@ export type getUsersProps = {
   updatedAt: Date;
   image: string | null;
 };
+
 export type profileUserInterface = Omit<
   NonNullable<CurrentUserProps['currentUser']>,
   'password' | 'emailVerified' | 'isAdmin'
@@ -94,6 +92,62 @@ export interface PostComponentInterface {
   isLiked: boolean;
   deletePost: any;
 }
+
+export interface PostInterface {
+  author?: authorType;
+  likes?: likesType[];
+  authorId?: string;
+  userId?: string;
+  id: string;
+  date: string;
+  content: string;
+  image_url: string;
+  commentCount: number;
+  likeCount: number;
+}
+
+export type PostCommentType = {
+  id: string;
+  userId: string;
+  name: string;
+  text: string;
+  profile_url?: string;
+  isLiked: boolean;
+  commentLikesCount: number;
+};
+
+export type editPostType = Pick<
+  PostInterface,
+  'id' | 'content' | 'image_url' | 'authorId'
+>;
+
+export type deletePostType = Pick<PostInterface, 'id' | 'authorId' | 'userId'>;
+
+export type fetchLikedPostsType = Pick<
+  PostComponentInterface,
+  'profileId' | 'userId'
+>;
+
+export type fetchPostType = Pick<PostComponentInterface, 'postId' | 'userId'>;
+
+export type fetchUserPostsType = {
+  start?: number;
+  id?: string | undefined | any;
+  userId: string | undefined;
+  authorId: string | undefined;
+};
+
+export type getSinglePost = {
+  userId: PostComponentInterface['userId'];
+  postId: string | string[];
+};
+
+export type toggleLikeType = {
+  postId: string | undefined;
+  userId: string;
+  commentId: string | undefined;
+};
+
 export interface CloudinaryResponseInterface {
   asset_id: string;
   public_id: string;
@@ -117,10 +171,7 @@ export interface CloudinaryResponseInterface {
   original_filename: string;
   api_key: string;
 }
-export type fetchLikedPostsType = Pick<
-  PostComponentInterface,
-  'profileId' | 'userId'
->;
+
 export type loadUsersType = {
   input?: string;
   userId?: string;
@@ -128,71 +179,24 @@ export type loadUsersType = {
   setUsers: (users: any) => void;
   onlyFollowed: boolean;
 };
+
 export type userServiceGetUsersProps = Pick<
   getUsersProps,
   'userId' | 'input' | 'followed'
 >;
-export type UpdateUserResponse = {
-  statusCode: number;
-  data: any; // you can type this later as User or whatever fits
-};
-export type UpdateUserArgs = {
-  form: FormData;
-  setUser: React.Dispatch<React.SetStateAction<User | null>>;
-};
+
 export type loadProfileType = Pick<getUsersProps, 'id'> & {
   setLoading: (boolean: boolean) => void;
   setUser: (data: any) => void;
   authorId: string;
 };
-export type getSinglePost = {
-  userId: PostComponentInterface['userId'];
-  postId: string | string[];
-};
-export type toggleLikeType = {
-  postId: string | undefined;
-  userId: string;
-  commentId: string | undefined;
-};
+
 export type followType = {
   isFollowed: boolean;
   followedId: string;
   followerId: string;
 };
-export type fetchPostType = Pick<PostComponentInterface, 'postId' | 'userId'>;
-export type fetchUserPostsType = {
-  start?: number;
-  id?: string | undefined | any;
-  userId: string | undefined;
-  authorId: string | undefined;
-};
 
-export type PostCommentType = {
-  text: string;
-  profile_url?: string;
-  isLiked: boolean;
-  commentLikesCount: number;
-  id: string;
-  userId: string;
-  name: string;
-};
-export interface PostInterface {
-  author?: authorType;
-  likes?: likesType[];
-  authorId?: string;
-  userId?: string;
-  id: string;
-  date: string;
-  content: string;
-  image_url: string;
-  commentCount: number;
-  likeCount: number;
-}
-export type editPostType = Pick<
-  PostInterface,
-  'id' | 'content' | 'image_url' | 'authorId'
->;
-export type deletePostType = Pick<PostInterface, 'id' | 'authorId' | 'userId'>;
 interface User {
   id: string;
   name: string;
@@ -203,9 +207,20 @@ export interface SessionInterface {
   user: User;
   status?: boolean;
 }
+
 export type LoginSchema = {
   name: string;
   password: string;
+};
+
+export type UpdateUserResponse = {
+  statusCode: number;
+  data: any;
+};
+
+export type UpdateUserArgs = {
+  form: FormData;
+  setUser: React.Dispatch<React.SetStateAction<User | null>>;
 };
 
 export type PopupState = {
@@ -213,15 +228,28 @@ export type PopupState = {
   text?: string;
   type?: 'success' | 'error' | 'loading';
 };
+
 export type PopupProps = {
   data: PopupState;
 };
+
 type ViewType = 'posts' | 'likes';
 
 export type PostsLikesToggleProps = {
   view: ViewType;
   setView: React.Dispatch<React.SetStateAction<ViewType>>;
 };
+
+type likesType = {
+  id: string;
+};
+
+type authorType = {
+  id: string;
+  image: string;
+  name: string;
+};
+
 export interface Author {
   id: string;
   name: string;
@@ -239,6 +267,7 @@ export interface singlePostInterface {
   author: Author;
   isLiked: boolean;
 }
+
 export interface CommentUser {
   id: string;
   name: string;
@@ -258,13 +287,17 @@ export interface CommentInterface {
   };
   isLiked: boolean;
 }
+
 export interface createCommentInterface {
-  commentLikesCount: number;
   id: string;
-  isLiked: boolean;
   post_id: string;
+  user_id: string;
   text: string;
   timestamp: string;
-  user: { image: string; name: string };
-  user_id: string;
+  commentLikesCount: number;
+  isLiked: boolean;
+  user: {
+    name: string;
+    image: string;
+  };
 }
