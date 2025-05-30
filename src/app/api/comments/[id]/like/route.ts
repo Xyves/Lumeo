d038e8961c21d838f189c818439bf7e15f23431b
Xyves <xodrixx@protonmail.com>
2025-05-30 11:35:58 +0200

    Chore: Update NextJS to 15.3 and delete tailwind scrollbar

--- src/app/api/comments/[id]/like/route.ts
+++ src/app/api/comments/[id]/like/route.ts
@@ -2,7 +2,10 @@ import { NextResponse } from 'next/server';
 
 import { handleCommentLikePrisma } from '@/services/postService';
 
-export async function POST(req: Request, { params }: { params: { id: string } }) {
+export async function POST(
+  req: Request,
+  { params }: { params: { id: string } }
+) {
   const { id } = await params;
   const { userId } = await req.json();
   try {
